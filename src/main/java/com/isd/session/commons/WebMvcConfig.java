fac3ff0f0a4a967524768d1f8eb4847d79ab95d9
Danilo Leocata <37438774+khalld@users.noreply.github.com>
2023-01-07 12:21:12 +0100

    Added dynamic port on WebMvcConfigurer

--- src/main/java/com/isd/session/commons/WebMvcConfig.java
+++ src/main/java/com/isd/session/commons/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.isd.session.commons;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -8,22 +9,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
 
-    private final String baseUrl = "http://localhost:8084";
+    private final String baseUrl = "http://localhost";
+    @Value("${server.port}")
+    private Integer port;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
-            .addResourceLocations("classpath:/META-INF/resources/");
+                .addResourceLocations("classpath:/META-INF/resources/");
 
         registry.addResourceHandler("/webjars/**")
-            .addResourceLocations("classpath:/META-INF/resources/webjars/");
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry
                 // SWAGGER
-                .addViewController(baseUrl + "/swagger-ui/")
+                .addViewController(baseUrl + ":" + port + "/swagger-ui/")
                 .setViewName("forward:" + baseUrl + "/swagger-ui/index.html");
     }
 }
